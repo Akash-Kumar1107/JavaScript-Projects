42f16542f89d7e38185127616bf7f8bc3f193754
Akash-Kumar1107 <122812784+Akash-Kumar1107@users.noreply.github.com>
2023-09-21 00:27:37 +0530

    Delete Simple JS Projects directory

--- Simple JS Projects/4. Digital Clock 🕒/Clock.js
+++
@@ -1,8 +0,0 @@
-const clock = document.getElementById('clock')
-//const clock = document.querySelector('#clock')
-
-setInterval(function(){
-    let date = new Date()
-    // console.log(date.toLocaleTimeString());
-    clock.innerHTML = date.toLocaleTimeString();
-}, 1000);
