59ac90d2efef222295dc8f369dd2963ccab24a21
Akash-Kumar1107 <122812784+Akash-Kumar1107@users.noreply.github.com>
2023-09-21 00:15:19 +0530

    Delete Simple JS Projects/10. Custom Cursor ⬆️ directory

--- Simple JS Projects/10. Custom Cursor ⬆️/script.js
+++
@@ -1,7 +0,0 @@
-var main = document.querySelector('#main')  
-var cursr = document.querySelector('.cursor')  
-
-main.addEventListener('mousemove',function (dets) {
-    cursr.style.left = dets.x+'px'
-    cursr.style.top = dets.y+'px'
-})
